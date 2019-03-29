6e61cf7cde5c4ac89f1a6c31172cce2eb7363fda
Felipe Litran <felipelitran@gmail.com>
2019-03-29 10:57:23 +0000

    New dataset and bootswatch theme implemented

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,35 +1,64 @@
-// To load the content from financialdata.csv we use the queue.js library
+// To load the content from winedata.csv we use the queue.js library
 queue()
-    .defer(d3.csv, "data/financialdata.csv")
+    .defer(d3.csv, "data/winedata.csv")
     .await(makeGraphs);
 
 
-// The data from the csv file will be passed to the variable financialData by queue.js
-function makeGraphs(error, financialData) {
+// The data from the csv file will be passed to the variable wineData by queue.js
+function makeGraphs(error, wineData) {
 
-    var ndx = crossfilter(financialData);
+    var ndx = crossfilter(wineData);
 
-    console.log(financialData);
+    console.log(wineData);
 
-    // Converting the financial dataset to integers as it is being currently read as text
-    financialData.forEach(function(d) {
+    // Converting the dataset to integers as it is being currently read as text
+    wineData.forEach(function(d) {
         d.price = parseInt(d["Price"]);
     });
 
-    show_price_per_share(ndx);
+    show_country_selector(ndx);
+    
+    show_variety_selector(ndx);
+    
+    show_sector_count(ndx);
 
     dc.renderAll();
 }
 
 
-function show_price_per_share(ndx) {
-    var dim = ndx.dimension(dc.pluck('Sector'));
+function show_country_selector(ndx) {
+
+    // Defining the variables
+    dim = ndx.dimension(dc.pluck('country'));
+    group = dim.group();
+
+    // Targeting the div which the chart will belong to
+    dc.selectMenu("#country-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+function show_variety_selector(ndx) {
+
+    // Defining the variables
+    dim = ndx.dimension(dc.pluck('variety'));
+    group = dim.group();
+
+    // Targeting the div which the chart will belong to
+    dc.selectMenu("#variety-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+
+function show_sector_count(ndx) {
+    var dim = ndx.dimension(dc.pluck('country'));
     var group = dim.group();
 
-    dc.barChart("#price-per-share")
-        .width(400)
+    dc.barChart("#sector-count")
+        .width(500)
         .height(300)
-        .margins({ top: 10, right: 50, bottom: 110, left: 50 })
+        .margins({ top: 30, right: 30, bottom: 60, left: 30 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -37,7 +66,7 @@ function show_price_per_share(ndx) {
         /*.elasticY(true)*/
         .xUnits(dc.units.ordinal)
         /*.xAxisLabel("Sector Name")*/
-        .yAxisLabel("Sector Count")
-        .yAxis().ticks(10);
+        /*.yAxisLabel("Sector Count")*/
+        .yAxis().ticks(11);
 
 }
